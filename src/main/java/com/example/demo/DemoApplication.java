3b55a06e9487180bca35fff292ceab1a80b7bb6b
bala-amypo <bala@amypo.in>
2025-12-21 13:19:52 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,12 @@
-spring.datasource.url=jdbc:mysql://localhost:3306/dynamic_pricing
-spring.datasource.username=root
-spring.datasource.password=root
+package com.example.demo;
 
-spring.jpa.hibernate.ddl-auto=update
-spring.jpa.show-sql=true
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-jwt.secret=secretkey
-jwt.expiration=86400000
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}

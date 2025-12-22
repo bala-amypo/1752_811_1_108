4648f0374fbbbb47680b0b23287cf3f796e49bf2
bala-amypo <bala@amypo.in>
2025-12-22 05:26:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/DynamicPriceRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.DynamicPriceRecord;
-
-public interface DynamicPriceRecordRepository
-        extends JpaRepository<DynamicPriceRecord, Long> {
-}

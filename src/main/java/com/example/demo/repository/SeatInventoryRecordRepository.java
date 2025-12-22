5d3bb853bb39fa86f2355f666c14b80c21aa7048
bala-amypo <bala@amypo.in>
2025-12-22 08:42:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/SeatInventoryRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.SeatInventoryRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatInventoryRecordRepository
-        extends JpaRepository<SeatInventoryRecord, Long> {
-}

12d0aa987ccd5c3fd1059a5b9bf5fe88d7e8b949
bala-amypo <bala@amypo.in>
2025-12-22 04:09:17 +0000

    User commit

--- src/main/java/com/example/demo/Entity/SeatInventoryRecord.java
+++ src/main/java/com/example/demo/Entity/SeatInventoryRecord.java
@@ -1,4 +1,4 @@
-package com.example.demo.model;
+package com.example.demo.entity;
 
 import jakarta.persistence.*;
 
@@ -9,6 +9,35 @@ public class SeatInventoryRecord {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private int totalSeats;
-    private int bookedSeats;
+    private Long eventId;
+    private Integer totalSeats;
+    private Integer availableSeats;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(Long eventId) {
+        this.eventId = eventId;
+    }
+
+    public Integer getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(Integer totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public Integer getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(Integer availableSeats) {
+        this.availableSeats = availableSeats;
+    }
 }

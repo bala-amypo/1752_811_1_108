d7f85c0491bf6d8ce3b4b9f2dccf1b531c731885
bala-amypo <bala@amypo.in>
2025-12-29 06:03:25 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HelloServlet.java
+++ src/main/java/com/example/demo/servlet/HelloServlet.java
@@ -3,14 +3,14 @@ package com.example.demo.servlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
 public class HelloServlet extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/plain");
-        resp.getWriter().write("Hello from Dynamic Event Ticket Pricing Engine Servlet");
+        resp.getWriter().write("Hello World");
+        resp.getWriter().flush();
     }
 }
